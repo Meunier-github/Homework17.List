8a912373fbb104d0d4e1b7c2348e58d2b8c4bdec
meunier-github <meunier.natalia@gmail.com>
2024-08-29 10:06:49 +0200

    Homework17.List

--- src/main/java/pro/sky/HomeworkList/EmployeeService.java
+++ src/main/java/pro/sky/HomeworkList/EmployeeService.java
@@ -1,12 +1,12 @@
 package pro.sky.HomeworkList;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 import pro.sky.HomeworkList.Exception.EmployeeAlreadyAddedException;
 import pro.sky.HomeworkList.Exception.EmployeeNotFoundException;
 import pro.sky.HomeworkList.Exception.EmployeeStorageIsFullException;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -14,8 +14,7 @@ public class EmployeeService implements HWListInterface {
     List<Employee> employees = new ArrayList<>();
     final int MAX_COUNT_EMPLOYEES = 5;
 
-    public Employee add(@RequestParam(value = "firstName") String firstName,
-                        @RequestParam(value = "lastName") String lastName) throws EmployeeStorageIsFullException, EmployeeAlreadyAddedException {
+    public Employee add(String firstName,String lastName) throws EmployeeStorageIsFullException, EmployeeAlreadyAddedException {
 
         if (employees.size() > MAX_COUNT_EMPLOYEES) {
             throw new EmployeeStorageIsFullException();
@@ -28,8 +27,7 @@ public class EmployeeService implements HWListInterface {
         return employee;
     }
 
-    public Employee remove(@RequestParam(value = "firstName") String firstName,
-                           @RequestParam(value = "lastName") String lastName) {
+    public Employee remove(String firstName,String lastName) {
 
 
         Employee employee = new Employee(firstName, lastName);
@@ -40,8 +38,7 @@ public class EmployeeService implements HWListInterface {
         return employee;
     }
 
-    public Employee find(@RequestParam(value = "firstName") String firstName,
-                         @RequestParam(value = "lastName") String lastName) {
+    public Employee find(String firstName,String lastName) {
         Employee employee = new Employee(firstName, lastName);
         if (!employees.contains(employee)) {
             throw new EmployeeNotFoundException();
@@ -50,7 +47,7 @@ public class EmployeeService implements HWListInterface {
     }
 
     public List print() {
-        return employees;
+        return Collections.unmodifiableList(employees);
     }
 
 }
--- src/main/java/pro/sky/HomeworkList/HWListInterface.java
+++ src/main/java/pro/sky/HomeworkList/HWListInterface.java
@@ -5,14 +5,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 public interface HWListInterface {
-    Employee add(@RequestParam(value = "firstName") String firstName,
-                 @RequestParam(value = "lastName") String lastName);
+    Employee add(String firstName,String lastName);
 
-    Employee remove(@RequestParam(value = "firstName") String firstName,
-                    @RequestParam(value = "lastName") String lastName);
+    Employee remove(String firstName,String lastName);
 
-    Employee find(@RequestParam(value = "firstName") String firstName,
-                  @RequestParam(value = "lastName") String lastName);
+    Employee find(String firstName,String lastName);
 
     List print();
 }
